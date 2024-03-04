3251664b4071303fccf8b5d846d9e267744b4593
Kylan Metzner <kylan.metzner@audencia.com>
2024-03-04 17:45:06 +0100

    first migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_154727) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_04_162318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clothes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "size"
+    t.string "category"
+    t.integer "value"
+    t.boolean "available"
+    t.string "condition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
