220a1fa20db2ef99f9423db7481289337e2cd5c9
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-03-05 18:21:27 +0100

    modification to seeds: addresses for users and one Clothe model 'robetest' with sizes array and condition array for random assigment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,43 +8,69 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+sizes = %w[XS S M L XL XXL XXXL]
+condition = ["bon état", "très bon état", "comme neuf"]
+
+
 User.destroy_all
 Clothe.destroy_all
 Transaction.destroy_all
 
 antoine = User.new({ user_name: "Antoinette", first_name: "Antoine", last_name: "Dupont", email: "antoine.dupont@email.com", phone_number: "0723456789", address: "11 rue du Chateau, 44000, Nantes", password: "123456", ranking: 4 })
+antoine.address = "18 Rue des Namnètes, 44840 Les Sorinières"
 antoine.save!
 
 kylan = User.new({ user_name: "Le Kycks", first_name: "Kylan", last_name: "Metzner", email: "kylan.metzner@email.com", phone_number: "0823456789", address: "15 rue du Bellevue, 44000, Nantes", password: "123456", ranking: 2 })
+kylan.address = "18 All. du Vigneau, 44840 Les Sorinières"
 kylan.save
 
 oscar = User.new({ user_name: "Oscarabé", first_name: "Oscar", last_name: "Pessans", email: "oscar.pessans@email.com", phone_number: "0734567890", address: "9 rue de Lamoricière, 44000, Nantes", password: "123456", ranking: 3 })
+oscar.address = "1 Rue des Charmilles, 44400 Rezé"
 oscar.save
 
 quentin = User.new({ user_name: "Quentintin", first_name: "Quentin", last_name: "Moussef", email: "quantin.moussef@email.com", phone_number: "0912345678", address: " 6 rue de la fosse aux lionn, 44000, Nantes", password: "123456", ranking: 3 })
+quentin.address = "5 Imp. de la Volière, 44400 Rezé"
 quentin.save
 
 juliette = User.new({ user_name: "Juju", first_name: "Juliette", last_name: "Simon", email: "juliette.sion@email.com", phone_number: "0923456789", address: "36 boulevard Jean Leduc, 44300, Nantes", password: "123456", ranking: 5 })
+juliette.address = "107 Rue du Château de Rezé, 44400 Rezé"
 juliette.save
 
 anna = User.new({ user_name: "Annana", first_name: "Anna", last_name: "Chelby", email: "anna.chelby@email.com", phone_number: "0634567890", address: "17 rue de la rivière, 44200, Nantes", password: "123456", ranking: 5 })
+anna.address = "11 Rue Saint-Hermeland, 44200 Nantes"
 anna.save
 
 pauline = User.new({ user_name: "Paulinette", first_name: "Pauline", last_name: "Assoun", email: "pauline.assoune@email.com", phone_number: "0912345678", address: "5 rue du temple,44100 , Nantes", password: "123456", ranking: 5 })
+pauline.address = "18 Rue Dobree, 44100 Nantes"
 pauline.save
 
 justine = User.new({ user_name: "Justiti", first_name: "Justine", last_name: "Assoun", email: "justine.assoun@email.com", phone_number: "0812345678", address: "95 ruelle du vieux chateau, 44300, Nantes", password: "123456", ranking: 4 })
+justine.address = "9 Rue de la Marne, 44000 Nantes"
 justine.save
 
 emma = User.new({ user_name: "Emmy", first_name: "Emma", last_name: "Lecombe", email: "emma.lecobe@email.com", phone_number: "0712345678", address: "1 boulevard de la bonne espérence, 44000, Nantes", password: "123456", ranking: 3 })
+emma.address = "10 Pass. Louis Lévesque, 44000 Nantes"
 emma.save
 
 ismael = User.new({ user_name: "Ismakan", first_name: "Ismael", last_name: "Tabyaoui", email: "ismael.tabyaoui@email.com", phone_number: "0612345678", address: "1 boulevard de la boulangerie, 44100, Nantes", password: "123456", ranking: 4})
+ismael.address ="16 Rue Georges Clemenceau, 44000 Nantes"
 ismael.save
 
 users = User.all
 users_ids = []
-
 users.each do |user|
   users_ids << user.id
 end
+
+robe_test = Clothe.new({  name: "Robe Lacoste noire",
+                          description: "ROBE POLO LACOSTE AVEC CEINTURE FEMME EN PETIT PIQUÉ SOUPLE BLEU MARINE Coupe ajustée en haut, évasée en bas Boutonnière intégrale en nacre Ceinture à nouer Crocodile vert brodé cousu sur la poitrine Coton (100%)",
+                          size: sizes.sample,
+                          category: "robe",
+                          value: "165",
+                          available: true,
+                          condition: condition.sample })
+
+robe_test.owner = ismael
+# file = File.open(Rails.root.join("app/assets/images/robe-test.jpg"))
+# robe_test.photos.attach(io: file, filename: "robe-test", content_type: "image/jpg")
+robe_test.save!
