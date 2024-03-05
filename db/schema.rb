5a8010884c7c9c54c738d5f48cc374717529516f
Kylan Metzner <kylan.metzner@audencia.com>
2024-03-05 16:26:30 +0100

    push of migrations resolved

--- db/schema.rb
+++ db/schema.rb
@@ -128,9 +128,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_143523) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "clothes", "users", column: "owner_id"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "clothes", "users", column: "owner_id"
   add_foreign_key "likes", "clothes"
   add_foreign_key "likes", "users"
   add_foreign_key "messages", "chatrooms"
