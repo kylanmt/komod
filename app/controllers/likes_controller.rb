730f7e2f4585efe76f7749ea576dab756ed30a55
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-03-07 12:22:17 +0100

    added button to go to profil of a user, liking a clothe and going to clothe show

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,8 +3,21 @@ class LikesController < ApplicationController
   end
 
   def create
+    clothe = Clothe.find(params[:clothe_id])
+    user = current_user
+    like = Like.new
+    like.clothe = clothe
+    like.user = user
+    like.save!
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:clothe_id)
   end
 end
