81881773c406e852d5ae9bc715e558447323d140
Oscar Pessans <oscar.pessans@audencia.com>
2024-03-14 15:45:58 +0100

    chatroom et messages terminés

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,6 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
   has_one :clothe_transaction, dependent: :destroy
+
+  has_many :clothes, through: :clothe_transaction
 end
